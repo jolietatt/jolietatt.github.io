203702dbfaf0342105a2f31d0cad07da352ab09e
jolietatt <a02273269@usu.edu>
2023-03-26 19:44:31 -0600

    Assignment 9
    Adding JavaScript

---
+++ javajam/js/verifysenior.js
@@ -0,0 +1,11 @@
+var age;
+
+age = prompt("Enter your age:")
+if (age >= 65) {
+    output = "Free Friday Coffee Night for Seniors!" 
+    document.getElementById('verify').style.color = 'maroon';
+} else {
+    output = "Enjoy Music and Make Memories!"
+}
+
+document.getElementById("verify").innerHTML = output;
